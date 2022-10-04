0850ab33d0be1f6a47b78b8edfc7420a4f5732a1
Asif Mehmood <asif-mehmood@hotmail.com>
2022-10-04 07:38:34 -0700

    1-16 some modifications

--- javascript 1-45/16.js
+++ javascript 1-45/16.js
@@ -11,16 +11,33 @@ guests[objIndex].name = "Khushbakh";
 
 console.log("Guests after sara left: ", guests);
 
-for (const [key, value] of Object.entries(guests)) {
-  console.log(`Your are invited Mr. ${value.name}`);
+function displayfunc() {
+  for (const [key, value] of Object.entries(guests)) {
+    console.log(`Your are invited Mr. ${value.name}`);
+  }
 }
 
-const obj = { id: 0, name: "asif" };
+displayfunc();
 
-guests.unshift(Object.assign(obj));
+const obj1 = { id: 0, name: "asif" }; //creating new object
 
-console.log(guests);
+guests.unshift(Object.assign(obj1));
 
-for (const [key, value] of Object.entries(guests)) {
-  console.log(`Your are invited Mr. ${value.name}`);
-}
+console.log("Added at the start\n", guests);
+
+displayfunc();
+
+const obj2 = { id: 10, name: "watto" }; //creating new object
+
+guests.push(Object.assign(obj2)); //adding element end of array
+
+console.log("Added at the end\n", guests);
+
+displayfunc();
+
+const obj3 = { id: 6, name: "shaur" }; //creating new object
+guests.splice(2, 0, Object.assign(obj3));
+
+console.log("Added at the middle\n", guests);
+
+displayfunc();
